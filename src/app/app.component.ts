84f4f5e0b070f6ec90391fbefe6d0fc89e511d09
Juan Rassa <jrassa@titamedia.com>
2023-01-20 09:01:21 -0500

    Following up of lecture 5.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
 })
+
 export class AppComponent {
-  title = 'VentaAutos';
+  title: string = 'Venta de Autos';
+  pais: string = "Colombia";
+  year: number = 1993;
 }
+
+
+
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'VentaAutos';
+// }
